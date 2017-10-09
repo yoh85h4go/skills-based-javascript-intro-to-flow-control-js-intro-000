5829722a9770d60204c458de1c7bb2552a1c9fa9
Young Oh <young.dan.oh@gmail.com>
2017-10-09 03:49:01 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age>=13&&age<=19)
+          {return "You are a teenager!"}
+  else if (age>19)
+          {return "You are a grownup"}
+  else if (age<13)
+          {return "You are a kid"}
 }
 
 function ternaryTeenager(age) {
