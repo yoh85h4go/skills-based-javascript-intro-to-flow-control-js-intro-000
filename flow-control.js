4979ee8c716160db1f7f18a0f5c0be4562cc0ebf
Young Oh <young.dan.oh@gmail.com>
2017-10-09 03:43:11 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function teenager(age) {
   if (age >= 13 && age <= 19)
         {return "You are a teenager!"}
 
-  else if (age>19) 
+  else if (age>19)
           {return "You are not a teenager"}
 
   else if (age<13)
