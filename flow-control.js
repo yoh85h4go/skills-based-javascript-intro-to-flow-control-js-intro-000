0bac596c9c3d77162c114a9e884a1a540ce9aaba
Young Oh <young.dan.oh@gmail.com>
2017-10-09 05:59:34 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -34,27 +34,27 @@ function switchAge(age)
   switch(age)
             {
                 case 13:
-                  console.log "You are a teenager";
+                  console.log (You are a teenager);
                   break;
                 case 14:
-                  console.log "You are a teenager";
+                  console.log (You are a teenager);
                   break;
                 case 15:
-                  console.log "You are a teenager"
+                  console.log (You are a teenager);
                   break;
                 case 16:
-                  console.log "You are a teenager"
+                  console.log (You are a teenager);
                   break;
                 case 17:
-                  console.log "You are a teenager"
+                  console.log (You are a teenager);
                   break;
                 case 18:
-                  console.log "You are a teenager"
+                  console.log (You are a teenager);
                   break;
                 case 19:
-                  console.log "You are a teenager"
+                  console.log (You are a teenager);
                   break;
                 default:
-                  console.log "You have an age"
+                  console.log(You have an age);
             }
 }
